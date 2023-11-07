cad02b91e70cb2251f0bff5144364fc52f2a52c0
Chase Forestello <chase.forestello@gmail.com>
2023-11-06 22:44:35 -0600

    Refactored to use papaparse. CSV parsing and PDF conversion working

--- src/components/uploader/Uploader.js
+++ src/components/uploader/Uploader.js
@@ -1,44 +1,98 @@
-import {useState, useEffect} from "react";
+import React, { useState } from 'react';
+import Papa from 'papaparse';
+import { PDFDocument, rgb, StandardFonts } from 'pdf-lib';
 
-const Uploader = () => {
-    const [file, setFile] = useState([]);
-    const [content, setContent] = useState();
+const allowedExtensions = ['csv'];
 
-    useEffect(() => {
-        setFile(null);
-        console.log(`File cleared: ${file}`);
-    }, [])
+const CsvToPdfConverter = () => {
+  const [error, setError] = useState('');
+  const [file, setFile] = useState(null);
 
-    async function readText(event) {
-        const file = event.target.files.item(0)
-        const text = await file.text();
+  const handleFileChange = (e) => {
+    setError('');
 
-        document.getElementById("output").innerText = text
+    if (e.target.files.length) {
+      const inputFile = e.target.files[0];
+      const fileExtension = inputFile?.type.split('/')[1];
+
+      if (!allowedExtensions.includes(fileExtension)) {
+        setError('Please input a CSV file');
+        return;
+      }
+
+      setFile(inputFile);
     }
+  };
 
-    useEffect(() => {
-        console.log(`Content: ${content}`);
-    }, [file]);
-    const handleChangeGetFile = () => {
-        console.log("Getting file");
-        const userFile = document.querySelector("#formFile").files;
-        setFile(userFile);
-    }
-    const readFile = async (uploadedFile) => {
-        console.log(`Uploaded file: ${uploadedFile}`);
-        const fileContent = await uploadedFile.text();
-        setContent(fileContent)
-    }
+  const handleParse = async () => {
+    if (!file) return alert('Enter a valid file');
 
-    return (
-        <div id={"uploader-wrapper"} className={"container"}>
-            <div className="mb-3">
-                <label htmlFor="formFile" className="form-label">Default file input example</label>
-                <input className="form-control" type="file" id="formFile" onChange={readText}/>
-            </div>
-            <div id={"output"} className={"card"}></div>
+    const reader = new FileReader();
+
+    reader.onload = async ({ target }) => {
+      const csv = Papa.parse(target.result, {
+        header: true,
+      });
+
+      const parsedData = csv?.data;
+
+      if (!parsedData || parsedData.length === 0) {
+        alert('No data found in the CSV file');
+        return;
+      }
+
+      // Filter only the specified columns
+      const filteredData = parsedData.map((row) => ({
+        Username: row['Username'],
+        'Time Label': row['Time Label'],
+        'Time Tracked': row['Time Tracked'],
+        'Task Name': row['Task Name'],
+        'User Total': row['User Total'],
+        'User Period': row['User Period'],
+      }));
+
+      await generatePDF(filteredData);
+    };
+
+    reader.readAsText(file);
+  };
+
+  const generatePDF = async (data) => {
+    const pdfDoc = await PDFDocument.create();
+    const page = pdfDoc.addPage([600, 400]);
+    const { width, height } = page.getSize();
+    const fontSize = 12;
+    const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
+
+    const content = data
+      .map((row) => Object.entries(row).map(([key, value]) => `${key}: ${value}`))
+      .join('\n');
+
+    page.drawText(content, {
+      x: 50,
+      y: height - 50,
+      size: fontSize,
+      font: timesRomanFont,
+      color: rgb(0, 0, 0),
+    });
+
+    const pdfBytes = await pdfDoc.save();
+    const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+    const url = window.URL.createObjectURL(blob);
+    window.open(url);
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        <input onChange={handleFileChange} id="csvInput" name="file" type="file" />
+        <div>
+          <button onClick={handleParse}>Convert to PDF</button>
         </div>
-    )
-}
+        {error ? error : null}
+      </div>
+    </div>
+  );
+};
 
-export default Uploader;
+export default CsvToPdfConverter;
